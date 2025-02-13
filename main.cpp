1b8637a07cf8cc06c85f75a11a491f37ffdb44a5
DeaisPMP <shishmintsev.d97@gmail.com>
2025-02-13 14:43:15 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <unordered_map>
 #include <queue>
 
-#ifndef __DEBUG__
+#if 0
 #define __DEBUG__
 #endif
 
@@ -14,21 +14,17 @@ class Stack
     {
         List* next;
         List* prev;
+        List* up;
         T     val;
         List()
-        : next(nullptr), prev(nullptr)
+        : next(nullptr), prev(nullptr), up(nullptr)
         {}
     };
     List*  head;
-    List*  tail;
-#ifdef __OLD__
-    std::map<T, int> depth;
-#endif
-    //std::map<T, std::queue<List*>> hash;
-    std::unordered_map<T, std::queue<List*>> hash;
+    std::unordered_map<T, List*> hash;
 public:
     Stack()
-    : head(nullptr), tail(nullptr)
+    : head(nullptr)
     {
         
     }
@@ -42,48 +38,47 @@ public:
         {
             head = new List();
             head->val = v;
-            tail = head;
-#ifdef __OLD__
-            depth[v]++;
-#endif
-            hash[v].push(head);
+            hash[v] = head;
             return;
         }
         head->next = new List();
         head->next->prev = head;
         head->next->val = v;
         head = head->next;
-#ifdef __OLD__
-        depth[v]++;
-#endif
-        hash[v].push(head);
+        auto f = hash.find(v);
+        if(f == hash.end())
+            hash[v] = head;
+        else
+        {
+            auto l = f->second;
+            while(l->up)
+                l = l->up;
+            l->up = head;
+        }
     }
     void pop_f()
     {
         if(head)
         {
-            auto& s = hash[head->val];
-            if(s.size() > 1)
+            auto s = hash[head->val];
+            if(s->up)
             {
-                auto del = s.front();
-                if(del != tail)
+                hash[head->val] = s->up;
+                if(s->prev) //s!=tail
                 {
-                    del->prev->next = del->next;
-                    del->next->prev = del->prev;
+                    s->prev->next = s->next;
+                    s->next->prev = s->prev;
                 }
                 else
-                {
-                    tail = del->next;
-                    tail->prev = nullptr;
-                }
-                delete del;
-                del = nullptr;
-                s.pop();
+                    s->next->prev = nullptr;
+                delete s;
+                s = nullptr;
             }
             else
             {
                 if(head->prev)
                 {
+                    hash.erase(s->val);
                     head = head->prev;
                     delete head->next;
                     head->next = nullptr;
@@ -92,62 +87,11 @@ public:
                 {
                     delete head;
                     head = nullptr;
-                    tail = nullptr;
-                }
-                s.pop();
-            }
-        }
-    }
-#ifdef __OLD__
-    void pop()
-    {
-        if(head)
-        {
-            int v = depth[head->val];
-            if(v > 1)
-            {
-                depth[head->val]--;
-                List* t_buf = tail;
-                while(t_buf)
-                {
-                    if(t_buf->val == head->val)
-                    {
-                        if(t_buf != tail)
-                        {
-                            t_buf->prev->next = t_buf->next;
-                            t_buf->next->prev = t_buf->prev;
-                            delete t_buf;
-                        }
-                        else
-                        {
-                            tail = t_buf->next;
-                            delete tail->prev;
-                            tail->prev = nullptr;
-                        }
-                        break;
-                    }
-                    t_buf = t_buf->next;
-                }
-            }
-            else
-            {
-                depth.erase(head->val);
-                if(head->prev)
-                {
-                    head = head->prev;
-                    delete head->next;
-                    head->next = nullptr;
-                }
-                else
-                {
-                    delete head;
-                    head = nullptr;
-                    tail = nullptr;
+                    hash.clear();
                 }
             }
         }
     }
-#endif
     T top()
     {
         if(head == nullptr)
@@ -193,11 +137,11 @@ int main(int argc, char* argv[])
     /*
     s.push(5);
     s.Print();
-    s.pop();
+    s.pop_f();
     s.Print();
     */
     
-    
+    /*
     s.push(5);
     s.Print();
     s.push(1);
@@ -242,6 +186,7 @@ int main(int argc, char* argv[])
     s.Print();
     s.pop_f();
     s.Print();
+    */
     
     return (0);
 }
